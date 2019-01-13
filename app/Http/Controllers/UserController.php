9296337f849b213e94fffa366904ab36e174d4ac
Marvin Birnbach <marvin@fam-birnbach.de>
2019-01-13 12:06:41 +0100

    ADD: missing users logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,33 +42,96 @@ class UserController extends Controller
 
     /**
      * List all users
+     * @return \Illuminate\Http\JsonResponse
      */
     public function list()
     {
-        //
+        $users = User::all();
+        $response = [
+            'users' => $users
+        ];
+        return response()->json($response, 200);
     }
 
     /**
      * Get user by Id
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function get()
+    public function get($id)
     {
-        //
+        $user = User::find($id);
+        $response = [
+            'user' => $user
+        ];
+        if(!$user)
+        {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        return response()->json($response, 200);
     }
 
     /**
      * Update a user by Id
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
-    public function update()
+    public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        if(!$user)
+        {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        $this->validate($request, [
+            'email' => 'email|unique:users'
+        ]);
+
+        if(isset($user->name))
+        {
+            $user->name = $request->input('name');
+        }
+        if(isset($user->username))
+        {
+            $user->username = $request->input('username');
+        }
+        if(isset($user->classId))
+        {
+            $user->classId = $request->input('classId');
+        }
+        if(isset($user->schulformId))
+        {
+            $user->schulformId = $request->input('schulformId');
+        }
+        if(isset($user->isAdmin))
+        {
+            $user->isAdmin = $request->input('isAdmin');
+        }
+        if(isset($user->email))
+        {
+            $user->email = $request->input('email');
+        }
+        if(isset($user->password))
+        {
+            $user->password = $request->input('password');
+        }
+
+        $user->save();
+        return response()->json(['user' => $user], 200);
     }
 
     /**
      * Delete a user by Id
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function delete()
+    public function delete($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(['message' => 'User deleted'], 200);
     }
 }
